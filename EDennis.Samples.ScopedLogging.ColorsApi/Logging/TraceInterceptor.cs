07ce0af73aec11933845364e51436db5892e89ec
Dennis Mitchell <Dennis.C.Mitchell@ct.gov>
2019-10-17 08:16:56 -0400

    Minor tweaks with TraceInterceptor

--- EDennis.Samples.ScopedLogging.ColorsApi/Logging/TraceInterceptor.cs
+++ EDennis.Samples.ScopedLogging.ColorsApi/Logging/TraceInterceptor.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace EDennis.Samples.ScopedLogging {
     public class TraceInterceptor : IInterceptor {
@@ -46,47 +47,75 @@ namespace EDennis.Samples.ScopedLogging {
                             logger.LogError(ex.InnerException, "For {User}, Exception {Method}(" + args + ") { Message}", User, method, ex.InnerException.Message);
                         throw ex.InnerException;
                     }
-                    if (invocation.ReturnValue != null)
-                        scope.Add("ReturnValue", invocation.ReturnValue.Format(false, false, false));
-                    logger.LogTrace("For {User}, Exit {Method}(" + args + ")", User, method);
+                    if (invocation.ReturnValue != null) {
+                        object returnValue = null;
+                        if (invocation.ReturnValue.GetType().IsGenericType && invocation.ReturnValue.GetType().GetGenericTypeDefinition() == typeof(Task<>))
+                            returnValue = ((dynamic)invocation.ReturnValue).Result;
+                        else returnValue = invocation.ReturnValue;
+                        var additionalScope = GetScope("ReturnValue", returnValue.Format());
+                        using (logger.BeginScope(additionalScope)) {
+                            logger.LogTrace("For {User}, Exit {Method}(" + args + ")", User, method);
+                        }
+                    } else {
+                        logger.LogTrace("For {User}, Exit {Method}(" + args + ")", User, method);
+                    }
                 }
             }
         }
 
 
-        private Dictionary<string, object> GetScope(IInvocation invocation) {
-            Dictionary<string, object> logScope = new Dictionary<string, object>();
-            var args = invocation.Arguments.Format(false, false, false);
+        private IEnumerable<KeyValuePair<string, object>> GetScope(IInvocation invocation) {
+            List<KeyValuePair<string, object>> logScope = new List<KeyValuePair<string, object>>();
+            var args = invocation.Arguments.Format();
             var parms = invocation.Method.GetParameters();
             for (int i = 0; i < parms.Length; i++) {
-                logScope.Add(parms[i].Name, args[i] ?? parms[i].DefaultValue);
+                logScope.Add(KeyValuePair.Create(parms[i].Name, args[i] ?? parms[i].DefaultValue));
             }
             return logScope;
+        }
+
+        private IEnumerable<KeyValuePair<string, object>> GetScope(string key, object value) {
+            var scope = new List<KeyValuePair<string, object>>();
+            scope.Add(KeyValuePair.Create(key, value));
+            return scope;
         }
 
     }
 
     public static class Extensions {
 
-        public static object[] Format(this object[] objs, bool compact = false, bool removeQuotes = false, bool escapeBraces = false) {
+        public static object[] Format(this object[] objs) {
             var retVal = new object[objs.Length];
             for (int i = 0; i < objs.Length; i++)
-                retVal[i] = objs[i].Format(compact, removeQuotes, escapeBraces);
+                retVal[i] = objs[i].Format();
             return retVal;
         }
 
-        public static string FormatCompact(this object[] objs) {
+        public static object Format(this object obj) {
+            object retVal;
+            if (obj.GetType().IsPrimitive)
+                retVal = obj;
+            else {
+                var str = JsonSerializer.Serialize(obj);
+                retVal = str;
+            }
+            return retVal;
+        }
+
+
+
+        public static string FormatCompact(this object[] objs, bool compact = true, bool removeQuotes = true, bool escapeBraces = true) {
             var sb = new StringBuilder();
 
             for (int i = 0; i < objs.Length; i++) {
                 if (i > 0)
                     sb.Append(",");
-                sb.Append(objs[i].Format());
+                sb.Append(objs[i].FormatCompact(compact, removeQuotes, escapeBraces));
             }
             return sb.ToString();
         }
 
-        public static object Format(this object obj, bool compact = true, bool removeQuotes = true, bool escapeBraces = true) {
+        public static object FormatCompact(this object obj, bool compact = true, bool removeQuotes = true, bool escapeBraces = true) {
             object retVal;
             if (obj.GetType().IsPrimitive)
                 retVal = obj;
